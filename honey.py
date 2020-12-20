3ac9c79c368e2dec8ca07a75fc3844469a55e862
Sanskriti <55370013+sanskriti35@users.noreply.github.com>
2020-12-20 20:11:26 +0530

    Update honey.py

--- honey.py
+++ honey.py
@@ -1,14 +1,14 @@
 import twilio
 from twilio.rest import Client 
  
-account_sid = 'AC24859f27775492518fce0f1068fe93d0' 
+account_sid = 's0mething' 
 auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' 
 client = Client(account_sid, auth_token) 
 def send_love():
 
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='You are the best mom in the world',      
+                              body='Hey! This will go directly and automatically. If you want me to help you with this, please let me know',      
                               to='whatsapp:+1555555555555' 
                           ) 
  
